8b0eb1dc27d87e211803db0e869baa3759b773c6
Xiao Song <xiaosx@berkeley.edu>
2022-04-13 12:14:08 -0700

    compare fix

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # ORB
-OpenCV compatable ORB feature detector used in ORB-SLAM1/2/3
+> OpenCV compatable ORB feature detector used in ORB-SLAM1/2/3
+>
+> This repo is part of UC Brekeley OpenARK Team2 capstone project
+
+
 
 ## File structure
+
 ```
 # Code header and implementation
 include/orb
@@ -9,9 +14,23 @@ src/
 
 # Example using customized orb feature
 examples/
+
+# Compare with opencv ORB and brisk
+compare/
 ```
 
+
+
+## Install Dependency
+
+1. CMake
+2. OpenCV 3.4.0 from github and build with CMake
+3. (optional, for compare only) BRISK. You can download brisk from here [link](http://github.com/xiaosong9905/brisk) where we have modify the brisk to be compatable with OpenCV 3.4.0 
+
+
+
 ## Build library
+
 ```shell
 cd orb # cd into current folder
 mkdir build 
@@ -24,6 +43,8 @@ make -j 10
 # Install package
 sudo make install 
 ```
+
+
 
 ## Reference
 
--- compare/compare.cc
+++ compare/compare.cc
@@ -5,6 +5,7 @@
 // 2. https://docs.opencv.org/3.4/dc/d16/tutorial_akaze_tracking.html
 // 
 #include <cstdio>
+#include <string>
 #include <vector>
 #include <memory> // shared_ptr
 #include <opencv2/opencv.hpp> // cv::Mat
@@ -12,60 +13,53 @@
 #include <brisk/brisk.h>
 #include <orb/orb.h> // Our own ORB package
 
-int main ( int argc, char** argv )
-{
-    if ( argc != 4 )
-    {
-        printf("Usage: ./demo PATH_TO_IMAGE1, PATH_TO_IMAGE2, DESCRIPTOR_TYPE\n");
-        exit(1);
-    }
-
-    detection_and_matching(argv);
-
-    return 0;
-}
-
 void detection_and_matching(char** argv) {
     cv::Mat image1 = cv::imread ( argv[1], cv::IMREAD_COLOR );
     cv::Mat image2 = cv::imread ( argv[2], cv::IMREAD_COLOR );
 
+    cv::Mat image1_grayscale;
+    cv::Mat image2_grayscale;
+
+    cv::cvtColor(image1, image1_grayscale, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(image2, image2_grayscale, cv::COLOR_BGR2GRAY);
+
     std::vector<cv::KeyPoint> keypoints1, keypoints2;
     cv::Mat descriptors1, descriptors2;
     
-    char* type = argv[3];
+    std::string type{ argv[3] };
     // detect and describe
     if (type == "orb_opencv") {
         printf("Build opencv ORB feature detector\n"); fflush( stdout );
         cv::Ptr<cv::ORB> orb = cv::ORB::create();
-        orb->detectAndCompute(image1, cv::Mat(), keypoints1, descriptors1);
-        orb->detectAndCompute(image2 cv::Mat(), keypoints2, descriptors2);
+        orb->detectAndCompute(image1, cv::noArray() , keypoints1, descriptors1);
+        orb->detectAndCompute(image2, cv::noArray(), keypoints2, descriptors2);
     } 
     else if (type == "brisk_opencv") {
         printf("Build opencv BRISK feature detector\n"); fflush( stdout );
         cv::Ptr<cv::BRISK> brisk = cv::BRISK::create();
-        brisk->detectAndCompute(image1, cv::Mat(), keypoints1, descriptors1);
-        brisk->detectAndCompute(image2 cv::Mat(), keypoints2, descriptors2);
+        brisk->detectAndCompute(image1, cv::noArray(), keypoints1, descriptors1);
+        brisk->detectAndCompute(image2, cv::noArray(), keypoints2, descriptors2);
     } 
     else if (type == "orb") {
         printf("Build our own ORB feature detector\n"); fflush( stdout );
         orb::ORBDetectorDescriptor orb_feature{ 100, 0.5, 10, 5, 2 }; // NOTE: those value are just random value used to compile the program
-        orb_feature.detectAndCompute( image1, cv::noArray(), keypoints1, descriptors1 );
-        orb_feature.detectAndCompute( image2, cv::noArray(), keypoints2, descriptors2 );
+        orb_feature.detectAndCompute( image1_grayscale, cv::noArray(), keypoints1, descriptors1 );
+        orb_feature.detectAndCompute( image2_grayscale, cv::noArray(), keypoints2, descriptors2 );
     }
-    else if (type == "brisk_openark") {
-        printf("Build OpenARK BRISK feature detector\n"); fflush( stdout );
+    else if (type == "brisk") {
+        printf("Build BRISK feature detector\n"); fflush( stdout );
         float threshold = 34.0;     // "The keypoint detector threshold."
         int octaves = 4;
         bool suppressScaleNonmaxima = true;
-        detector = new brisk::BriskFeatureDetector(threshold, octaves, suppressScaleNonmaxima);
-        detector.detect(image1, keypoints1);
-        detector.detect(image2, keypoints2);
+        auto detector = new brisk::BriskFeatureDetector(threshold, octaves, suppressScaleNonmaxima);
+        detector->detect(image1_grayscale, keypoints1);
+        detector->detect(image2_grayscale, keypoints2);
 
         bool rotationInvariant = true;  // "If set to false, keypoints are assumed upright."
         bool scaleInvariant = true;     // "If set to false, keypoints are all assigned the same scale."
-        descriptorExtractor = new brisk::BriskDescriptorExtractor(rotationInvariant, scaleInvariant, brisk::BriskDescriptorExtractor::Version::briskV2);
-        descriptorExtractor->compute(image1, keypoints1, descriptors1);
-        descriptorExtractor->compute(image2, keypoints2, descriptors2);
+        auto descriptorExtractor = new brisk::BriskDescriptorExtractor(rotationInvariant, scaleInvariant, brisk::BriskDescriptorExtractor::Version::briskV2);
+        descriptorExtractor->compute(image1_grayscale, keypoints1, descriptors1);
+        descriptorExtractor->compute(image2_grayscale, keypoints2, descriptors2);
     }
 
     // Matching
@@ -81,4 +75,17 @@ void detection_and_matching(char** argv) {
         cv::Scalar(0, 255, 0), cv::Scalar(0, 0, 255), \
         std::vector<std::vector<char> >(), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
     cv::imwrite("match_image.png", image_matches);
+}
+
+int main ( int argc, char** argv )
+{
+    if ( argc != 4 )
+    {
+        printf("Usage: ./demo PATH_TO_IMAGE1, PATH_TO_IMAGE2, DESCRIPTOR_TYPE\n");
+        exit(1);
+    }
+
+    detection_and_matching(argv);
+
+    return 0;
 }
